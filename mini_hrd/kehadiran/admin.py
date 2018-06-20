92846127a65eac5e9a1dfe1599a15fefccf264ba
brokilham <plato.ilham@gmail.com>
2018-06-20 14:18:38 +0700

    membuat model  dan menampilkan di admin

--- mini_hrd/kehadiran/admin.py
+++ mini_hrd/kehadiran/admin.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from kehadiran.models import Kehadiran
+
 # Register your models here.
+class KehadiranAdmin (admin.ModelAdmin):
+    list_display = ['karyawan', 'jenis_kehadiran', 'waktu']
+    list_filter = ('jenis_kehadiran',)
+    search_fields = []
+    list_per_page = 25
+
+admin.site.register(Kehadiran, KehadiranAdmin)
